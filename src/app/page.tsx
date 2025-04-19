31084f17cf7158e82b00a67c5d666b6165f410d1
kuzyameduzya <qashke33@gmail.com>
2025-04-19 19:55:21 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import styles from "./page.module.css";
 import EmblaCarousel from "@/components/Carousel/Carousel";
 import { EmblaOptionsType } from "embla-carousel";
 import { TrailerInfo } from "@/types";
-import TrailerCard from "@/components/TrailerCard/TrailerCard";
 
 export default function Home() {
   const OPTIONS: EmblaOptionsType = {};
@@ -13,18 +12,6 @@ export default function Home() {
     "/images/zavod/zavod04.jpg",
     "/images/zavod/zavod05.jpg",
   ];
-
-  const trailer: TrailerInfo = {
-    title: "TSR 5SOU2N",
-    subtitle: "extendable semi-trailer",
-    axles: 5,
-    capacity: "64,5",
-    length: "16,6 – 24,6",
-    extension: "8",
-    workingArea: "11,8 – 19,8",
-    loadingHeight: "895",
-    imageUrl: "/images/5osei/01.webp",
-  };
 
   return (
     <div className={styles.page}>
