2b39e847219dc3d6964f49559cc9d70e08bb1e67
kuzyameduzya <qashke33@gmail.com>
2025-04-19 17:19:48 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,20 @@
 import type { Metadata } from "next";
+import { Inter, Unbounded } from "next/font/google";
 import "./globals.css";
 
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getLocale, getTranslations } from "next-intl/server";
 import Header from "@/components/Header";
+
+const interFont = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const unboundedFont = Unbounded({
+  subsets: ["latin"],
+  variable: "--font-unbounded",
+});
 
 export async function generateMetadata(): Promise<Metadata> {
   const t = await getTranslations("Layout");
@@ -23,7 +34,7 @@ export default async function RootLayout({
   const messages = await getMessages();
   return (
     <html lang={locale}>
-      <body>
+      <body className={`${interFont.variable} ${unboundedFont.variable}`}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Header />
           {children}
